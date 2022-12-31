2951e206e008376ee7b25e51a5d20ddf4303d0f6
Aki-100 <aki.y.0406@icloud.com>
2022-12-31 13:09:38 +0900

    5件ごとのペジネーションの作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
